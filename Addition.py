321388ca5977b216f49cb05c6c8f0b883546ccb3
aappy <aparnachdhry@gmail.com>
2021-02-18 23:50:07 -0800

    python tests

---
+++ Addition.py
@@ -0,0 +1,5 @@
+class Add:
+    @staticmethod
+    def addMethod(num1 , num2):
+        return num1 + num2
+
---
+++ test_Addition.py
@@ -0,0 +1,5 @@
+from Addition import Add
+
+
+def test_add_method():
+    assert Add.addMethod(10 , 20) == 30
